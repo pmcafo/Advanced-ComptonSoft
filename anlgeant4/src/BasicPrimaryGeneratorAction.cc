283ce05ec1069a0bdfa377211576dcaacecbe797
pmcafo <pmcafo@proton.me>
2021-08-14 15:12:55 +0000

    more moe

--- anlgeant4/src/BasicPrimaryGeneratorAction.cc
+++ anlgeant4/src/BasicPrimaryGeneratorAction.cc
@@ -12,4 +12,70 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  * GNU General Public License for more details.                          *
  *                                                                       *
- * You should have received a copy of the GNU General Public L
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                       *
+ *************************************************************************/
+
+#include "BasicPrimaryGeneratorAction.hh"
+
+#include "G4ParticleGun.hh"
+#include "G4ParticleTable.hh"
+#include "AstroUnits.hh"
+#include "BasicPrimaryGen.hh"
+
+namespace anlgeant4
+{
+
+BasicPrimaryGeneratorAction::BasicPrimaryGeneratorAction()
+  : m_ParticleGun(new G4ParticleGun(1)),
+    m_Time(0.0), m_Position(0.0, 0.0, 0.0),
+    m_Energy(10.0*unit::keV), m_Direction(1.0, 0.0, 0.0)
+{
+}
+
+BasicPrimaryGeneratorAction::BasicPrimaryGeneratorAction(G4ParticleDefinition* definition)
+  : m_ParticleGun(new G4ParticleGun(1)),
+    m_Time(0.0), m_Position(0.0, 0.0, 0.0),
+    m_Energy(10.0*unit::keV), m_Direction(1.0, 0.0, 0.0)
+{
+  m_ParticleGun->SetParticleDefinition(definition);
+}
+
+BasicPrimaryGeneratorAction::BasicPrimaryGeneratorAction(G4String particle_name)
+  : m_ParticleGun(new G4ParticleGun(1)),
+    m_Time(0.0), m_Position(0.0, 0.0, 0.0),
+    m_Energy(10.0*unit::keV), m_Direction(1.0, 0.0, 0.0)
+{
+  G4ParticleDefinition* particle = 
+    G4ParticleTable::GetParticleTable()->FindParticle(particle_name);
+  m_ParticleGun->SetParticleDefinition(particle);
+}
+
+BasicPrimaryGeneratorAction::~BasicPrimaryGeneratorAction() = default;
+
+void BasicPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
+{
+  if (m_GeneratorSetting) {
+    m_GeneratorSetting->makePrimarySetting();
+    m_GeneratorSetting->confirmPrimarySetting();
+  }
+
+  m_ParticleGun->SetParticleTime(m_Time);
+  m_ParticleGun->SetParticlePosition(m_Position);
+  m_ParticleGun->SetParticleEnergy(m_Energy);
+  m_ParticleGun->SetParticleMomentumDirection(m_Direction);
+  if (m_Polarization.x()!=0.0 ||
+      m_Polarization.y()!=0.0 ||
+      m_Polarization.z()!=0.0) {
+    m_ParticleGun->SetParticlePolarization(m_Polarization);
+  }
+  m_ParticleGun->GeneratePrimaryVertex(anEvent);
+}
+
+void BasicPrimaryGeneratorAction::SetDefinition(G4ParticleDefinition* definition)
+{
+  m_ParticleGun->SetParticleDefinition(definition);
+}
+
+} /* namespace anlgeant4 */
---
+++ anlgeant4/src/Geant4Body.cc
@@ -0,0 +1,231 @@
+
+/*************************************************************************
+ *                                                                       *
+ * Copyright (c) 2011 Shin Watanabe, Hirokazu Odaka                      *
+ *                                                                       *
+ * This program is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                       *
+ *************************************************************************/
+
+#include "Geant4Body.hh"
+
+#include <ctime>
+#include <boost/lexical_cast.hpp>
+
+#include "ANLG4RunManager.hh"
+
+#include "G4VUserDetectorConstruction.hh"
+#include "G4VUserPhysicsList.hh"
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4UImanager.hh"
+
+#include "VANLGeometry.hh"
+#include "VANLPhysicsList.hh"
+#include "VANLPrimaryGen.hh"
+#include "VUserActionAssembly.hh"
+
+using namespace anlnext;
+
+namespace anlgeant4
+{
+
+Geant4Body::Geant4Body()
+  : m_G4RunManager(new ANLG4RunManager),
+    m_EventIndex(0),
+    m_RandomEngine("MTwistEngine"),
+    m_RandomInitMode(1),
+    m_RandomSeed1(0),
+    m_OutputRandomStatus(true),
+    m_RandomInitialStatusFileName("RandomSeed_i.dat"),
+    m_RandomFinalStatusFileName("RandomSeed_f.dat"),
+    m_VerboseLevel(0)
+{
+}
+
+Geant4Body::~Geant4Body() = default;
+
+ANLStatus Geant4Body::mod_define()
+{
+  register_parameter(&m_RandomEngine, "random_engine");
+  register_parameter(&m_RandomInitMode, "random_initialization_mode");
+  set_parameter_question("Random initialization mode (0: auto, 1: interger, 2: state file)");
+  register_parameter(&m_RandomSeed1, "random_seed");
+  register_parameter(&m_OutputRandomStatus, "output_random_status");
+  register_parameter(&m_RandomInitialStatusFileName,
+                     "random_initial_status_file");
+  register_parameter(&m_RandomFinalStatusFileName,
+                     "random_final_status_file");
+
+  register_parameter(&m_VerboseLevel, "verbose");
+  register_parameter(&m_UserCommands, "commands");
+  
+  return AS_OK;
+}
+
+ANLStatus Geant4Body::mod_initialize()
+{
+  if (m_RandomInitMode==0 || m_RandomInitMode==1 || m_RandomInitMode==2) {
+    initialize_random_generator();
+  }
+  else {
+    std::cout << "Invalid value [Random initialization mode] : "
+              << m_RandomInitMode << std::endl;
+    return AS_QUIT_ERROR;
+  }
+
+  set_user_initializations();
+  set_user_primary_generator_action();
+  set_user_defined_actions();
+  apply_commands();
+
+  m_G4RunManager->Initialize();
+
+  return AS_OK;
+}
+
+void Geant4Body::initialize_random_generator()
+{
+  if (m_RandomEngine=="MTwistEngine") {
+    m_RandomEnginePtr.reset(new CLHEP::MTwistEngine);
+    CLHEP::HepRandom::setTheEngine(m_RandomEnginePtr.get());
+  }
+  else if (m_RandomEngine=="RanecuEngine") {
+    m_RandomEnginePtr.reset(new CLHEP::RanecuEngine);
+    CLHEP::HepRandom::setTheEngine(m_RandomEnginePtr.get());
+  }
+  else if (m_RandomEngine=="HepJamesRandom") {
+    m_RandomEnginePtr.reset(new CLHEP::HepJamesRandom);
+    CLHEP::HepRandom::setTheEngine(m_RandomEnginePtr.get());
+  }
+  
+  if (m_RandomInitMode==0) {
+    m_RandomSeed1 = std::time(0);
+    CLHEP::HepRandom::setTheSeed(m_RandomSeed1);
+    std::cout << "Random seed: " << m_RandomSeed1 << std::endl;
+
+    if (m_OutputRandomStatus) {
+      CLHEP::HepRandom::saveEngineStatus(m_RandomInitialStatusFileName.c_str());
+    }
+  }
+  else if (m_RandomInitMode==1) {
+    CLHEP::HepRandom::setTheSeed(m_RandomSeed1);
+    std::cout << "Random seed: " << m_RandomSeed1 << std::endl;
+
+    if (m_OutputRandomStatus) {
+      CLHEP::HepRandom::saveEngineStatus(m_RandomInitialStatusFileName.c_str());
+    }
+  }
+  else if (m_RandomInitMode==2) {
+    CLHEP::HepRandom::restoreEngineStatus(m_RandomInitialStatusFileName.c_str());
+  }
+}
+
+void Geant4Body::set_user_initializations()
+{
+  VANLGeometry* geometry;
+  get_module_NC("VANLGeometry", &geometry);
+  G4VUserDetectorConstruction* userDetectorConstruction = geometry->create();
+  m_G4RunManager->SetUserInitialization(userDetectorConstruction);
+  
+  VANLPhysicsList* physics;
+  get_module_NC("VANLPhysicsList", &physics);
+  G4VUserPhysicsList* userPhysicsList = physics->create();
+  m_G4RunManager->SetUserInitialization(userPhysicsList);
+}
+
+void Geant4Body::set_user_primary_generator_action()
+{
+  VANLPrimaryGen* primaryGen;
+  get_module_NC("VANLPrimaryGen", &primaryGen);
+  G4VUserPrimaryGeneratorAction* userPrimaryGeneratorAction
+    = primaryGen->create();
+  m_G4RunManager->SetUserAction(userPrimaryGeneratorAction);
+}
+
+void Geant4Body::set_user_defined_actions()
+{
+  VUserActionAssembly* userActionAssembly;
+  get_module_NC("VUserActionAssembly", &userActionAssembly);
+  userActionAssembly->registerUserActions(m_G4RunManager.get());
+}
+
+void Geant4Body::apply_commands()
+{
+  using boost::format;
+  using boost::str;
+
+  G4UImanager* ui = G4UImanager::GetUIpointer();
+  
+  std::vector<std::string> presetCommands;
+  presetCommands.push_back( str(format("/run/verbose %d") % m_VerboseLevel) );
+  presetCommands.push_back( str(format("/event/verbose %d") % m_VerboseLevel) );
+  presetCommands.push_back( str(format("/tracking/verbose %d") % m_VerboseLevel) );
+
+  std::cout << "\nApplying preset commands:" << std::endl;
+  for (const std::string& com: presetCommands) {
+    std::cout << com << std::endl;
+    ui->ApplyCommand(com);
+  }
+
+  std::cout << "\nApplying user commands:" << std::endl;
+  for (const std::string& com: m_UserCommands) {
+    std::cout << com << std::endl;
+    ui->ApplyCommand(com);
+  }
+  std::cout << std::endl;
+}
+
+ANLStatus Geant4Body::mod_begin_run()
+{
+  const G4bool cond = m_G4RunManager->ConfirmBeamOnCondition();
+  if (cond) {
+    m_G4RunManager->ConstructScoringWorlds();
+    m_G4RunManager->RunInitialization();
+    m_G4RunManager->InitializeEventLoop(0, nullptr, 0);
+  }
+  else {
+    return AS_QUIT_ERROR;
+  }
+
+  return AS_OK;
+}
+
+ANLStatus Geant4Body::mod_analyze()
+{
+  const ANLStatus status = m_G4RunManager->performOneEvent(m_EventIndex);
+  ++m_EventIndex;
+
+  return status;
+}
+
+ANLStatus Geant4Body::mod_end_run()
+{
+  m_G4RunManager->TerminateEventLoop();
+  m_G4RunManager->RunTermination();
+
+  return AS_OK;
+}
+
+ANLStatus Geant4Body::mod_finalize()
+{
+  if (m_OutputRandomStatus) {
+    CLHEP::HepRandom::saveEngineStatus(m_RandomFinalStatusFileName.c_str());
+  }
+
+  m_G4RunManager.reset(nullptr);
+
+  return AS_OK;
+}
+
+} /* namespace anlgeant4 */
