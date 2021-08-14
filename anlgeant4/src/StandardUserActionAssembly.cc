0674559f761ba40fcebb5572a4b1f47701f0ec08
pmcafo <pmcafo@proton.me>
2021-08-14 16:58:50 +0000

    Rename some tests

---
+++ anlgeant4/src/StandardUserActionAssembly.cc
@@ -0,0 +1,56 @@
+/*************************************************************************
+ *                                                                       *
+ * Copyright (c) 2011 Hirokazu Odaka                                     *
+ *                                                                       *
+ * This program is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                       *
+ *************************************************************************/
+
+#include "StandardUserActionAssembly.hh"
+#include "G4Event.hh"
+#include "InitialInformation.hh"
+
+using namespace anlnext;
+
+namespace anlgeant4
+{
+
+StandardUserActionAssembly::StandardUserActionAssembly()
+  : m_InitialInfo(0)
+{
+  add_alias("StandardUserActionAssembly");
+}
+
+ANLStatus StandardUserActionAssembly::mod_initialize()
+{
+  get_module_IFNC("InitialInformation", &m_InitialInfo);
+  return AS_OK;
+}
+
+void StandardUserActionAssembly::EventActionAtBeginning(const G4Event* anEvent)
+{
+  m_InitialInfo->setEventID(anEvent->GetEventID());
+}
+
+void StandardUserActionAssembly::setInitialTime(double v)
+{
+  m_InitialInfo->setInitialTime(v);
+}
+
+double StandardUserActionAssembly::getInitialTime() const
+{
+  return m_InitialInfo->InitialTime();
+}
+
+} /* namespace anlgeant4 */
---
+++ anlgeant4/src/UserActionAssemblyEventAction.cc
@@ -0,0 +1,48 @@
+
+/*************************************************************************
+ *                                                                       *
+ * Copyright (c) 2011 Shin Watanabe, Hirokazu Odaka                      *
+ *                                                                       *
+ * This program is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                       *
+ *************************************************************************/
+
+#include "UserActionAssemblyEventAction.hh"
+#include "VUserActionAssembly.hh"
+
+namespace anlgeant4
+{
+
+UserActionAssemblyEventAction::UserActionAssemblyEventAction(const std::list<VUserActionAssembly*>& userActions)
+  : userActions_(userActions)
+{
+}
+ 
+UserActionAssemblyEventAction::~UserActionAssemblyEventAction() = default;
+
+void UserActionAssemblyEventAction::BeginOfEventAction(const G4Event* anEvent)
+{
+  for (VUserActionAssembly* pud: userActions_) {
+    pud->EventActionAtBeginning(anEvent);
+  }
+}
+
+void UserActionAssemblyEventAction::EndOfEventAction(const G4Event* anEvent)
+{
+  for (VUserActionAssembly* pud: userActions_) {
+    pud->EventActionAtEnd(anEvent);
+  }
+}
+
+} /* namespace anlgeant4 */
